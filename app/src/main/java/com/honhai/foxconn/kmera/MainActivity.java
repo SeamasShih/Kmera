cb25db4c3936533181b165bf216c70c062882d22
SeamasShih <seamas0716@gmail.com>
2018-09-07 08:30:14 +0800

    Initial commit

---
+++ app/src/main/java/com/honhai/foxconn/kmera/MainActivity.java
@@ -0,0 +1,13 @@
+package com.honhai.foxconn.kmera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
