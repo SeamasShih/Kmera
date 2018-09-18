8ecdc2a5fd57bf36ad7f60ca602332cf3418a142
ALiao1432 <apple36578@gmail.com>
2018-09-18 10:45:33 +0800

    move math method to Mathematics

--- app/src/main/java/com/honhai/foxconn/kmera/Tools/Mathematics.java
+++ app/src/main/java/com/honhai/foxconn/kmera/Tools/Mathematics.java
@@ -11,4 +11,12 @@ public class Mathematics {
         }
         return (float) Math.toDegrees(Math.atan(dy/dx));
     }
+
+    public static boolean isInner(int target, int centre) {
+        return isInner(target,centre,45);
+    }
+
+    public static boolean isInner(int target, int centre , int tolerance) {
+        return Math.abs((target - centre) % 360) < tolerance;
+    }
 }
